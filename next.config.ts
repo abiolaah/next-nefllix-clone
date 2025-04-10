1a3a6a506e35401a16c1840cb818267db84f98a5
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-10 17:21:45 -0600

    Refactor to support image source

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "uhdtv.io",
+      "mango.blender.org",
+      "download.blender.org",
+      "www.cartoonbrew.com",
+      "commondatastorage.googleapis.com",
+    ],
+  },
 };
 
 export default nextConfig;
