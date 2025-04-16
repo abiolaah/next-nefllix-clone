7b646e8f0554cfccf1e55e95abcca533405b6a68
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-16 13:56:55 -0600

    Refactor file for horizontal scroll

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -8,9 +8,13 @@ interface MovieData {
   description: string;
   videoUrl: string;
   thumbnailUrl: string;
-  genre: string;
+  genre: string[];
   rating: number | null;
   duration: string;
+  releaseDate?: string;
+  adult?: boolean;
+  isTvShow?: boolean;
+  numberOfSeasons?: number;
 }
 
 interface MovieListProps {
@@ -25,14 +29,18 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
 
   return (
     <div className="px-4 md:px-12 mt-4 space-y-8">
-      <div className="">
+      <div>
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
           {title}
         </p>
-        <div className="grid grid-cols-4 gap-2">
-          {data.map((movie) => (
-            <MovieCard key={movie.id} data={movie} />
-          ))}
+        <div className="relative">
+          <div className="flex space-x-4 overflow-x-scroll scrollbar-hide pb-4">
+            {data.map((movie) => (
+              <div key={movie.id} className="flex-none">
+                <MovieCard data={movie} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
