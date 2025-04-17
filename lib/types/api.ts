eaf05bea6f22cec1b9b6b2a8cf7f40c4ef17c91e
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-16 23:49:26 -0600

    Refactor file for updated type declaration

--- lib/types/api.ts
+++ lib/types/api.ts
@@ -1,18 +1,38 @@
 import { Movie, TvShow } from "@prisma/client";
 
+export interface MovieData {
+  id: string;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  trailerUrl: string;
+  genre: string[];
+  rating: number | null;
+  duration: string;
+}
+
+export interface TvShowData {
+  id: string;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  trailerUrl: string;
+  genre: string[];
+  rating: number | null;
+  numberOfSeasons: number;
+}
+
 export interface TransformedMovie extends Omit<Movie, "id"> {
   id: string;
   genre: string[];
-  releaseDate: string;
-  adult: boolean;
   isTvShow: false;
 }
 
 export interface TransformedTvShow extends Omit<TvShow, "id"> {
   id: string;
   genre: string[];
-  releaseDate: string;
-  adult: boolean;
   isTvShow: true;
 }
 
