f981e0b5d8e7170afa0f68addd19c995412b4550
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-16 23:47:49 -0600

    Refactor file to include functions to ensure both data from TMDB API and MOngoDB match

--- lib/transform.ts
+++ lib/transform.ts
@@ -1,14 +1,6 @@
 import { TMDB_IMAGE_BASE_URL, TMDB_IMAGE_SIZES } from "./tmdb";
-
-interface TMDBMovie {
-  id: number;
-  title: string;
-  overview: string;
-  poster_path: string | null;
-  backdrop_path: string | null;
-  release_date: string;
-  vote_average: number;
-}
+import { TMDBMovie, TMDBTvShow } from "./types/tmdb";
+import { MediaItem } from "./types/api";
 
 interface LocalMovie {
   id: string;
@@ -19,6 +11,10 @@ interface LocalMovie {
   genre: string;
   rating: number | null;
   duration: string;
+}
+
+interface ExtendedTMDBTvShow extends TMDBTvShow {
+  episode_run_time?: number[];
 }
 
 const getImageUrl = (path: string | null): string => {
@@ -32,6 +28,39 @@ const getImageUrl = (path: string | null): string => {
   return `${TMDB_IMAGE_BASE_URL}${TMDB_IMAGE_SIZES.POSTER.ORIGINAL}/${cleanPath}`;
 };
 
+export const transformMovie = (movie: TMDBMovie): MediaItem => {
+  return {
+    id: movie.id.toString(),
+    title: movie.title,
+    description: movie.overview,
+    videoUrl: "",
+    thumbnailUrl: getImageUrl(movie.poster_path),
+    trailerUrl: "",
+    genre: movie.genre_ids.map((id) => id.toString()),
+    rating: movie.vote_average,
+    duration: `${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}m`,
+    isTvShow: false,
+    source: "tmdb",
+  };
+};
+
+export const transformTvShow = (show: ExtendedTMDBTvShow): MediaItem => {
+  return {
+    id: show.id.toString(),
+    title: show.name,
+    description: show.overview,
+    videoUrl: "",
+    thumbnailUrl: getImageUrl(show.poster_path),
+    trailerUrl: "",
+    genre: show.genre_ids.map((id) => id.toString()),
+    rating: show.vote_average,
+    duration: `${show.episode_run_time?.[0] || 45}m`,
+    isTvShow: true,
+    numberOfSeasons: show.number_of_seasons,
+    source: "tmdb",
+  };
+};
+
 export const transformTMDBToLocal = (tmdbMovie: TMDBMovie): LocalMovie => {
   return {
     id: tmdbMovie.id.toString(),
@@ -39,20 +68,23 @@ export const transformTMDBToLocal = (tmdbMovie: TMDBMovie): LocalMovie => {
     description: tmdbMovie.overview,
     videoUrl: "", // TMDB doesn't provide video URLs
     thumbnailUrl: getImageUrl(tmdbMovie.poster_path),
-    genre: "", // You might want to map TMDB genres to your local genres
+    genre: tmdbMovie.genre_ids.join(", "),
     rating: tmdbMovie.vote_average,
-    duration: "", // TMDB provides runtime in minutes, you might want to format it
+    duration: `${Math.floor(tmdbMovie.runtime / 60)}h ${
+      tmdbMovie.runtime % 60
+    }m`,
   };
 };
 
-export const transformLocalToTMDB = (localMovie: LocalMovie): TMDBMovie => {
+export const transformLocalToTMDB = (
+  localMovie: LocalMovie
+): Partial<TMDBMovie> => {
   return {
     id: parseInt(localMovie.id),
     title: localMovie.title,
     overview: localMovie.description,
-    poster_path: localMovie.thumbnailUrl.split("/").pop() || null,
-    backdrop_path: null, // You might want to store this in your local database
-    release_date: "", // You might want to store this in your local database
+    poster_path: localMovie.thumbnailUrl.split("/").pop() || "",
+    release_date: "",
     vote_average: localMovie.rating || 0,
   };
 };
