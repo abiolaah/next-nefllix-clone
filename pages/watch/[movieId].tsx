918d2869e532ae3a35f758f40ab9c9e58ec51f63
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-21 17:08:53 -0600

    Refactor to remove video controls and ensure movie play

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -1,3 +1,4 @@
+//Resolve why video is not playing
 import React, { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import useMovieDetails from "@/hooks/useMovieDetails";
