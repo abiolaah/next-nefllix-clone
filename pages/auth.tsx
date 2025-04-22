74ae4858b99463145c9f868e034d3ea386ba3725
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-21 22:53:31 -0600

    Refactor to ensure proper login routing

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -9,8 +9,11 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 import Input from "@/components/Input";
+import { useRouter } from "next/router";
 
 const Auth = () => {
+  const router = useRouter();
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -25,16 +28,24 @@ const Auth = () => {
 
   const login = useCallback(async () => {
     try {
-      await signIn("credentials", {
+      const result = await signIn("credentials", {
         email,
         password,
         redirect: false,
         callbackUrl: "/profiles",
       });
+
+      if (result?.error) {
+        console.log(result.error);
+      }
+
+      if (result?.url) {
+        router.push(result.url);
+      }
     } catch (error) {
       console.log(error);
     }
-  }, [email, password]);
+  }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
