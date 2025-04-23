6ec31de47caff935be81bd733455bd6d518c6e12
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-22 23:00:12 -0600

    Refactor file to display movie list based on various type

--- pages/browse/index.tsx
+++ pages/browse/index.tsx
@@ -5,7 +5,6 @@ import Navbar from "@/components/Navbar";
 import useInfoModal from "@/hooks/useInfoModal";
 import useMovies from "@/hooks/useMovies";
 import useTvShows from "@/hooks/useTvShows";
-import { MediaItem } from "@/lib/types/api";
 
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
@@ -29,11 +28,53 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const Browse = () => {
-  const { data: movies, isLoading: moviesLoading } = useMovies();
-  const { data: tvShows, isLoading: tvShowsLoading } = useTvShows();
+  const { data: movies, isLoading: moviesLoading } = useMovies({
+    type: "trending",
+  });
+  const { data: popularMovies, isLoading: popularMoviesLoading } = useMovies({
+    type: "popular",
+  });
+  const { data: topRatedMovies, isLoading: topRatedMoviesLoading } = useMovies({
+    type: "top_rated",
+  });
+  const { data: nowPlayingMovies, isLoading: nowPlayingMoviesLoading } =
+    useMovies({ type: "now_playing" });
+  const { data: trendingMovies, isLoading: trendingMoviesLoading } = useMovies({
+    type: "trending",
+    page: 2,
+  });
+  const { data: upcomingMovies, isLoading: upcomingMoviesLoading } = useMovies({
+    type: "upcoming",
+  });
+  const { data: tvShows, isLoading: tvShowsLoading } = useTvShows({
+    type: "trending",
+  });
+  const { data: popularTvShows, isLoading: popularTvShowsLoading } = useTvShows(
+    { type: "popular", page: 2 }
+  );
+  const { data: onAirTvShows, isLoading: onAirTvShowsLoading } = useTvShows({
+    type: "on_air",
+  });
+  const { data: topRatedTvShows, isLoading: topRatedTvShowsLoading } =
+    useTvShows({ type: "top_rated" });
+  const { data: tredningTvShows, isLoading: tredningTvShowsLoading } =
+    useTvShows({ type: "trending", page: 2 });
+
   const { isOpen, closeModal } = useInfoModal();
 
-  if (moviesLoading || tvShowsLoading) {
+  if (
+    moviesLoading ||
+    popularMoviesLoading ||
+    topRatedMoviesLoading ||
+    trendingMoviesLoading ||
+    nowPlayingMoviesLoading ||
+    upcomingMoviesLoading ||
+    tvShowsLoading ||
+    onAirTvShowsLoading ||
+    topRatedTvShowsLoading ||
+    tredningTvShowsLoading ||
+    popularTvShowsLoading
+  ) {
     return (
       <>
         <Navbar />
@@ -45,19 +86,6 @@ const Browse = () => {
     );
   }
 
-  // Combine local and TMDB data
-  const allMovies: MediaItem[] = [
-    ...(movies?.local || []),
-    ...(movies?.tmdb || []),
-  ];
-
-  const allTvShows: MediaItem[] = [
-    ...(tvShows?.local || []),
-    ...(tvShows?.tmdb || []),
-  ];
-
-  const allMedia: MediaItem[] = [...allMovies, ...allTvShows];
-
   return (
     <>
       <InfoModal visible={isOpen} onClose={closeModal} />
@@ -68,13 +96,36 @@ const Browse = () => {
           title="Movies Only on This Platform"
           data={movies?.local || []}
         />
-        <MovieList title="Popular Movies" data={movies?.tmdb || []} />
+        <MovieList title="Popular Movies" data={popularMovies?.tmdb || []} />
+        <MovieList title="Trending Movies" data={trendingMovies?.tmdb || []} />
+        <MovieList title="Top rated Movies" data={topRatedMovies?.tmdb || []} />
+        <MovieList
+          title="Now Playing Movies"
+          data={nowPlayingMovies?.tmdb || []}
+        />
+        <MovieList title="Upcoming Movies" data={upcomingMovies?.tmdb || []} />
         <MovieList
           title="TV Shows Only on This Platform"
           data={tvShows?.local || []}
         />
-        <MovieList title="Popular TV Shows" data={tvShows?.tmdb || []} />
-        <MovieList title="Top 10" data={allMedia} />
+        <MovieList title="Popular TV Shows" data={popularTvShows?.tmdb || []} />
+        <MovieList
+          title="Top Rated TV Shows"
+          data={topRatedTvShows?.tmdb || []}
+        />
+        <MovieList
+          title="Trending TV Shows"
+          data={tredningTvShows?.tmdb || []}
+        />
+        <MovieList title="On Air TV Shows" data={onAirTvShows?.tmdb || []} />
+        <MovieList
+          title="Top 10 Movies"
+          data={(movies?.tmdb || []).slice(0, 10)}
+        />
+        <MovieList
+          title="Top 10 TV Shows"
+          data={(tvShows?.tmdb || []).slice(0, 10)}
+        />
       </div>
     </>
   );
