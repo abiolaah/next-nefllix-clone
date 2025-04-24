afa7ec20951b987a332d5695bc4d0ed65d2f26c2
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-23 19:11:19 -0600

    Refactor to include additional image hostname

--- next.config.ts
+++ next.config.ts
@@ -61,6 +61,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "mediaproxy.tvtropes.org",
       },
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
     ],
   },
   devIndicators: false,
