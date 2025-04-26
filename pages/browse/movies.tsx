14e38871eb28b7dd77e932ffdc09cec2a719e7f1
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-26 02:49:37 -0600

    Refactor for restyling and data rendering

--- pages/browse/movies.tsx
+++ pages/browse/movies.tsx
@@ -1,9 +1,14 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
-import Navbar from "@/components/Navbar";
+
+import Billboard from "@/components/Billboard";
+import InfoModal from "@/components/InfoModal";
 import MovieList from "@/components/MovieList";
+import Navbar from "@/components/Navbar";
+import TopMovieList from "@/components/TopMovieList";
+
+import useInfoModal from "@/hooks/useInfoModal";
 import useMovies from "@/hooks/useMovies";
-import { CategoryResponse } from "@/lib/types/api";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,16 +28,80 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Movies() {
-  const { data: moviesData = {} } = useMovies();
-  const movies = moviesData as CategoryResponse;
+  const { data: movies, isLoading: moviesLoading } = useMovies({
+    type: "trending",
+  });
+  const { data: popularMovies, isLoading: popularMoviesLoading } = useMovies({
+    type: "popular",
+  });
+  const { data: topRatedMovies, isLoading: topRatedMoviesLoading } = useMovies({
+    type: "top_rated",
+  });
+  const { data: nowPlayingMovies, isLoading: nowPlayingMoviesLoading } =
+    useMovies({ type: "now_playing" });
+  const { data: trendingMovies, isLoading: trendingMoviesLoading } = useMovies({
+    type: "trending",
+    page: 2,
+  });
+  const { data: upcomingMovies, isLoading: upcomingMoviesLoading } = useMovies({
+    type: "upcoming",
+  });
+
+  const { isOpen, closeModal } = useInfoModal();
+
+  if (
+    moviesLoading ||
+    popularMoviesLoading ||
+    topRatedMoviesLoading ||
+    trendingMoviesLoading ||
+    nowPlayingMoviesLoading ||
+    upcomingMoviesLoading
+  ) {
+    return (
+      <>
+        <Navbar />
+        <Billboard />
+        <div className="pb-40">
+          <div className="text-white">Loading...</div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
-      <div className="pt-20">
-        {Object.entries(movies).map(([category, items]) => (
-          <MovieList key={category} title={category} data={items} />
-        ))}
+      <Billboard />
+      <div className="pb-20">
+        {/* Movies Available on this platform */}
+        <MovieList
+          title="Movies Only on This Platform"
+          data={movies?.local || []}
+        />
+        {/* Popular Movies */}
+        <MovieList title="Popular Movies" data={popularMovies?.tmdb || []} />
+
+        {/* Trending Movies */}
+        <MovieList title="Trending Movies" data={trendingMovies?.tmdb || []} />
+
+        {/* Top Rated Movies */}
+        <MovieList title="Top Rated Movies" data={topRatedMovies?.tmdb || []} />
+
+        {/* Movies Now Playing */}
+        <MovieList
+          title="Now Playing Movies"
+          data={nowPlayingMovies?.tmdb || []}
+        />
+
+        {/* Upcoming Movies */}
+        <MovieList title="Upcoming Movies" data={upcomingMovies?.tmdb || []} />
+
+        {/* Top 10 Movies */}
+        <TopMovieList
+          title="Top 10 Movies"
+          data={(movies?.tmdb || []).slice(0, 10)}
+        />
       </div>
     </>
   );
