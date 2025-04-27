ea8c0ee21dc3de48520052a712c722e7c7c143c4
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-26 18:17:43 -0600

    Refactor to include source

--- hooks/useFavourites.ts
+++ hooks/useFavourites.ts
@@ -1,16 +1,21 @@
+// hooks/useFavourites.ts
 import useSWR from "swr";
-
 import fetcher from "@/lib/fetcher";
 import { MediaItem } from "@/lib/types/api";
 
+type FavoritesItems = MediaItem & {
+  source: "local" | "tmdb";
+};
+
 const useFavourites = (profileId?: string) => {
-  const { data, isLoading, error, mutate } = useSWR<MediaItem[]>(
+  const { data, error, isLoading, mutate } = useSWR<FavoritesItems[]>(
     profileId ? `/api/favourites?profileId=${profileId}` : null,
     fetcher,
     {
       revalidateIfStale: false,
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
+      shouldRetryOnError: false, // Prevent infinite retries on 500 errors
     }
   );
 
