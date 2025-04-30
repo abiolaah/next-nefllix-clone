6eef6eb88ad89a51d6a221a00bf28f68e6a43fce
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-30 12:35:39 -0600

    Refactor to include DELETE and PUT handler methods

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -45,6 +45,70 @@ export default async function handler(
       return res.status(200).json(profiles);
     }
 
+    if (req.method === "DELETE") {
+      const { profileId } = req.query;
+
+      // Ensure profileId provided is valid
+      if (!profileId || typeof profileId !== "string") {
+        return res.status(400).json({ error: "Invalid profile ID" });
+      }
+
+      // For all methods, verify the profile belongs to the current user
+      const profile = await prismadb.profile.findUnique({
+        where: {
+          id: profileId,
+        },
+      });
+
+      if (!profile || profile.userId !== currentUser.id) {
+        return res.status(403).json({ error: "Unauthorized" });
+      }
+
+      // Delete the profile
+      await prismadb.profile.delete({
+        where: {
+          id: profileId,
+        },
+      });
+
+      return res.status(200).json({ success: true });
+    }
+
+    if (req.method === "PUT") {
+      const { profileId } = req.query;
+      const { name, pin } = req.body;
+
+      // Ensure profileId provided is valid
+      if (!profileId || typeof profileId !== "string") {
+        return res.status(400).json({ error: "Invalid profile ID" });
+      }
+
+      // For all methods, verify the profile belongs to the current user
+      const profile = await prismadb.profile.findUnique({
+        where: {
+          id: profileId,
+        },
+      });
+
+      if (!profile || profile.userId !== currentUser.id) {
+        return res.status(403).json({ error: "Unauthorized" });
+      }
+
+      // Delete the profile
+      const updatedProfile = await prismadb.profile.update({
+        where: {
+          id: profileId,
+        },
+        data: {
+          name,
+          pin,
+          hasPin: pin ? true : false,
+        },
+      });
+
+      return res.status(200).json(updatedProfile);
+    }
+
     return res.status(405).end();
   } catch (error) {
     console.log(error);
